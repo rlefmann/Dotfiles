e74a33ca8f7521a8db9248af44a317a781c1388b
Raoul Lefmann <=>
2019-08-01 21:39:34 +0200

    delete .bash_profile, install.sh creates symbolic link instead

--- .bash_profile
+++
@@ -1,6 +0,0 @@
-# .bash_profile
-
-[ -f $HOME/.profile ] && . $HOME/.profile
-
-# Get the aliases and functions
-[ -f $HOME/.bashrc ] && . $HOME/.bashrc
--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ command -v rsync >/dev/null 2>&1 || { echo >&2 "Error: rsync is not installed.  
 
 rsync --exclude ".git/" --exclude "README.md" --exclude "install.sh" -avh . $HOME
 
+# Remove .bash_profile and add a symbolic link with the same name to .profile
+# This ensures that .profile is read by every bash instance
+rm -f "$HOME/.bash_profile"
+ln -s "$HOME/.profile" "$HOME/.bash_profile"
