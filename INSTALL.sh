77d931f89d1c4c7c8896f1f2bff8e5f383f0e265
Raoul Lefmann <raoul.lefmann@gmail.com>
2020-04-09 10:49:26 +0200

    Replaced copy command in installation script

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,16 +13,16 @@ if [ $yn != "y" ] && [ $yn != "Y" ]; then
 	exit 1
 fi
 
-cp .bashrc  ~
-cp .profile ~
-cp .xinitrc ~
-cp .Xresources ~
-
 # Create directories:
 find .config -type d -links 2 -exec mkdir -p "$HOME/{}" \;
 
 # Copy files:
-find .config -type f -exec cp "{}" "$HOME/{}" \;
+find . \
+	-type f \
+  -not -path "*/.git/*" \
+	-not -name "INSTALL.sh" \
+	-not -name "README.md" \
+	-exec cp "{}" "$HOME/{}" \;
 
 # Remove .bash_profile and add a symbolic link with the same name to .profile
 # This ensures that .profile is read by every bash instance
